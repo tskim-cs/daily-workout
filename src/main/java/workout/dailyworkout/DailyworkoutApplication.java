36fc8d07c49ee663ee806fbc261ae140728e3db2
Taeksoo Kim <ts.kim@kaist.ac.kr>
2021-01-16 18:34:24 +0900

    Init commit

---
+++ src/main/java/workout/dailyworkout/DailyworkoutApplication.java
@@ -0,0 +1,13 @@
+package workout.dailyworkout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyworkoutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyworkoutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/workout/dailyworkout/DailyworkoutApplicationTests.java
@@ -0,0 +1,13 @@
+package workout.dailyworkout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyworkoutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
