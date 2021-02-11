f92bcfcab3daec98107b1b6e2eef648c9225c960
Taeksoo Kim <ts.kim@kaist.ac.kr>
2021-02-12 00:08:28 +0900

    Add POST test for Exercise API

--- src/test/java/workout/dailyworkout/api/ExerciseApiControllerTest.java
+++ src/test/java/workout/dailyworkout/api/ExerciseApiControllerTest.java
@@ -1,9 +1,11 @@
 package workout.dailyworkout.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import workout.dailyworkout.domain.Exercise;
 import workout.dailyworkout.domain.ExerciseEquip;
@@ -14,6 +16,7 @@ import java.util.Collections;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -32,6 +35,16 @@ public class ExerciseApiControllerTest {
     public void contextLoads() throws Exception {
         assertNotNull(mockMvc);
         assertNotNull(exerciseService);
+    }
+
+    private String asJsonString(final Object obj) {
+        try {
+            final ObjectMapper mapper = new ObjectMapper();
+            final String jsonContent = mapper.writeValueAsString(obj);
+            return jsonContent;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private void createNewExercise(String name, ExerciseType type, ExerciseEquip equip) {
@@ -67,5 +80,27 @@ public class ExerciseApiControllerTest {
                 .andExpect(jsonPath("$.name").value("Barbell Squat"));
     }
 
+    @Test
+    public void saveExercise_postRequest() throws Exception {
+        // Given
+        when(exerciseService.addExercise(any(Exercise.class))).thenReturn(1L);
+
+        // When
+        ExerciseApiController.CreateExerciseRequest request = new ExerciseApiController.CreateExerciseRequest();
+        request.setName("Barbell squat");
+        request.setEquip("BARBELL");
+        request.setType("LEGS");
+
+        mockMvc.perform(post("/api/v1/exercises")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(request))
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.name").value("Barbell Squat"));
+    }
+
     // TODO - Get Wrong Param Name Exception Test
+    // TODO - Post Duplicated Exercise Name Exception Test
 }
